46b667336937945bb459939664770ac14cbd8eae
maximilian <maximilian.ki04@gmail.com>
2021-05-07 23:53:34 +0200

    Decorator entfernt/ ersetzt, game-logic läuft fehlerfrei
    Bug fixes, Decorators mit Singleton für Gitternetz ersetzt, generierung von Goal in  Raum hinzugefügt

--- gitternetz.ts
+++ gitternetz.ts
@@ -1,104 +1,115 @@
-import {isElem, isElem1, shuffle} from "./hilfsfunktionen.js"
-import {RoomIn} from "./raum.js"
+import { isElem, isElem1, shuffle } from "./hilfsfunktionen.js"
+import { RoomIn } from "./raum.js"
 
 
 export class GitterNetz {
-    hoch: number;
-    breit: number;
-    rooms: Array<[number, RoomIn]>;
-    edges: Array<[number, number, boolean]>;
-    ende:number
-    start:number
-    constructor(hoch:number, breit:number) {
-      this.hoch = hoch;
-      this.breit = breit;
-      this.rooms = [];
-      for (let i =0; i < this.breit*this.hoch; i++){
-        let broom = new RoomIn();
-        this.rooms.push([i, broom]);
-      }	
-      this.edges = [];
-      for (let i = 0; i < (this.hoch*this.breit); i++) {
-        if ((!((i%this.breit)+1===this.breit))&&(!(isElem([i,i+1,false], this.edges)))) {
-          this.edges.push([i,i+1,false])
-        }
-        if ((!(i>(this.breit*(this.hoch-1)-1)))&&(!(isElem([i,i+this.breit,false], this.edges)))) {
-          this.edges.push([i,i+this.breit,false])
-        }
+  private static gitter: GitterNetz;
+  hoch: number;
+  breit: number;
+  rooms: Array<[number, RoomIn]>;
+  edges: Array<[number, number, boolean]>;
+  ende: number
+  start: number
+  private constructor(hoch: number, breit: number) {
+    this.hoch = hoch;
+    this.breit = breit;
+    this.rooms = [];
+    for (let i = 0; i < this.breit * this.hoch; i++) {
+      let broom = new RoomIn();
+      if (i == this.coordsToRoomNum([this.ende, this.breit-1])) {
+        broom.goal = true;
       }
-      this.start = Math.round(Math.random()*(this.hoch-1))
-      this.ende = Math.round(Math.random()*this.hoch)
-      this.dfs(5)
+      this.rooms.push([i, broom]);
     }
-    roomOfIndex(i:number) {
-      for (var ecke of this.rooms) {
-        if (ecke[0] === i) {return ecke[1]}
+    this.edges = [];
+    for (let i = 0; i < (this.hoch * this.breit); i++) {
+      if ((!((i % this.breit) + 1 === this.breit)) && (!(isElem([i, i + 1, false], this.edges)))) {
+        this.edges.push([i, i + 1, false])
+      }
+      if ((!(i > (this.breit * (this.hoch - 1) - 1))) && (!(isElem([i, i + this.breit, false], this.edges)))) {
+        this.edges.push([i, i + this.breit, false])
       }
     }
-    roomNumToCoords(i:number):[number, number] {
-      return [(Math.floor(i/(this.breit))), i%(this.breit)]
+    this.start = Math.round(Math.random() * (this.hoch - 1))
+    this.ende = Math.round(Math.random() * this.hoch)
+    this.dfs(5)
+  }
+
+  public static generateGitternetz(hoch, breit): GitterNetz {
+    if (!GitterNetz.gitter) {
+      GitterNetz.gitter = new GitterNetz(hoch, breit)
     }
-    coordsToRoomNum([x,y]:[number,number]) {
-      return this.breit*y+x
+    return GitterNetz.gitter
+  }
+  roomOfIndex(i: number) {
+    for (var ecke of this.rooms) {
+      if (ecke[0] === i) { return ecke[1] }
     }
-    findneighbours(i:number) {
-      let raumliste:number[] = []
-      if (!(i<=this.breit)) {raumliste.push(this.rooms[(i-this.breit)][0])}
-      if (!(i%this.breit === 0)) {raumliste.push(this.rooms[(i-1)][0])}
-      if (!(i%this.breit===this.breit-1)) {raumliste.push(this.rooms[(i+1)][0])}
-      if (!(i>=(this.hoch-1)*this.breit)) {raumliste.push(this.rooms[(i+this.breit)][0])}
-      return raumliste
-    }
-    dfs(probability:number = 5, start:number=0, way:number[] = [0]) {
-        let next:number[] = shuffle(this.findneighbours(start))
-        for (let room of next) {
-            let in_way = isElem1(room, way)
-            let chance = Math.random()*100 <= probability;
-            if (!in_way || chance) {
-                this.edges.forEach(function(value) {if((value[0] === start && value[1] === room) || (value[1] === start && value[0] === room)) {value[2] = true}})
-                if (!in_way) {
-                    way.push(room)
-                    this.dfs(probability, room, way);
-                }
-            }
+  }
+  roomNumToCoords(i: number): [number, number] {
+    return [(Math.floor(i / (this.breit))), i % (this.breit)]
+  }
+  coordsToRoomNum([x, y]: [number, number]) {
+    return this.breit * y + x
+  }
+  findneighbours(i: number) {
+    let raumliste: number[] = []
+    if (!(i <= this.breit)) { raumliste.push(this.rooms[(i - this.breit)][0]) }
+    if (!(i % this.breit === 0)) { raumliste.push(this.rooms[(i - 1)][0]) }
+    if (!(i % this.breit === this.breit - 1)) { raumliste.push(this.rooms[(i + 1)][0]) }
+    if (!(i >= (this.hoch - 1) * this.breit)) { raumliste.push(this.rooms[(i + this.breit)][0]) }
+    return raumliste
+  }
+  dfs(probability: number = 5, start: number = 0, way: number[] = [0]) {
+    let next: number[] = shuffle(this.findneighbours(start))
+    for (let room of next) {
+      let in_way = isElem1(room, way)
+      let chance = Math.random() * 100 <= probability;
+      if (!in_way || chance) {
+        this.edges.forEach(function (value) { if ((value[0] === start && value[1] === room) || (value[1] === start && value[0] === room)) { value[2] = true } })
+        if (!in_way) {
+          way.push(room)
+          this.dfs(probability, room, way);
         }
-    }
-    show() {
-      let strE: string = "|"
-      let strS: string = " " 
-      let strB: string = "_"
-      let row = strE
-      let str = ""
-      for (let i = 0; i < this.breit; i++) {
-        str = str.concat(strS.toString())
-        str = str.concat(strB.toString())
       }
-      console.log(str)
-      strS = " "
-      for (let i = 0; i < this.hoch*this.breit; i++) {
-        let currentrow = Math.floor(i/this.breit)
-        if (isElem([i, i+this.breit, false], this.edges)||i>(this.breit*(this.hoch-1))-1) {
-          row = row.concat(strB.toString())
-        } else {
-          row = row.concat(strS.toString())
-        }
-        if (isElem([i, i+1, false], this.edges)) {
+    }
+  }
+  show() {
+    let strE: string = "|"
+    let strS: string = " "
+    let strB: string = "_"
+    let row = strE
+    let str = ""
+    for (let i = 0; i < this.breit; i++) {
+      str = str.concat(strS.toString())
+      str = str.concat(strB.toString())
+    }
+    console.log(str)
+    strS = " "
+    for (let i = 0; i < this.hoch * this.breit; i++) {
+      let currentrow = Math.floor(i / this.breit)
+      if (isElem([i, i + this.breit, false], this.edges) || i > (this.breit * (this.hoch - 1)) - 1) {
+        row = row.concat(strB.toString())
+      } else {
+        row = row.concat(strS.toString())
+      }
+      if (isElem([i, i + 1, false], this.edges)) {
+        row = row.concat(strE.toString())
+      } else {
+        row = row.concat(strS.toString())
+      }
+      if ((i + 1) % this.breit === 0) {
+        row = row.substring(0, row.length - 1)
+        if (!(currentrow === this.ende)) {
           row = row.concat(strE.toString())
-        } else {
-          row = row.concat(strS.toString())
         }
-        if ((i+1)%this.breit=== 0) {
-          row = row.substring(0, row.length - 1)
-          if (!(currentrow===this.ende)) {
-            row = row.concat(strE.toString())
-          }
-          console.log(row)
-          if (currentrow===this.start) {
-            row = " "
-          } else {
-            row = "|"
-          }
+        console.log(row)
+        if (currentrow === this.start) {
+          row = " "
+        } else {
+          row = "|"
         }
       }
     }
+  }
 }
--- spiel.ts
+++ spiel.ts
@@ -1,91 +1,93 @@
-import {isElem} from "./hilfsfunktionen.js"
-import {Status} from "./status.js"
-import {RoomIn, Room} from "./raum.js"
-import {GitterNetz} from "./gitternetz.js"
+import { isElem } from "./hilfsfunktionen.js"
+import { Status } from "./status.js"
+import { RoomIn, Room } from "./raum.js"
+import { GitterNetz } from "./gitternetz.js"
 
 
-enum Direction{
+enum Direction {
   Up,
   Down,
   Left,
   Right
 }
 
-export default class Spiel{
-  private agents:[number, number, number][]; // [[raumIndex, HP, attackDMG], ...]
-  private current_agent:number;
+export default class Spiel {
+  private agents: [number, number, number][]; // [[raumIndex, HP, attackDMG], ...]
+  private current_agent: number;
   private g: GitterNetz;
 
   constructor(hoch, breit) {
     this.agents = []
     this.current_agent = -1
-    this.g = new GitterNetz(hoch, breit)
+    this.g = GitterNetz.generateGitternetz(hoch, breit)
+    this.g.show()
   }
-  createAgent():number {
+  createAgent(): number {
     this.agents.push([this.g.coordsToRoomNum([this.g.start, 0]), 30, 3])
-    this.current_agent ++
+    this.current_agent++
     return this.current_agent
   }
-  
-  neighbours(id:number):(Room | undefined)[] {
-      let i = this.agents[id][0]
-      let raumliste: (Room | undefined)[] = [undefined, undefined, undefined, undefined]
-      if ((isElem([i, i-this.g.breit, true], this.g.edges))) {raumliste[0] = this.g.rooms[i-this.g.breit][1].getRoom()}
-      if ((isElem([i, i+1, true], this.g.edges))) {raumliste[1] = this.g.rooms[i+1][1].getRoom()}
-      if ((isElem([i, i+this.g.breit, true], this.g.edges))) {raumliste[2] = this.g.rooms[i+this.g.breit][1].getRoom()}
-      if ((isElem([i, i-1, true], this.g.edges))) {raumliste[3] = this.g.rooms[i-1][1].getRoom()}
-      return raumliste
+
+  neighbours(id: number): (Room | undefined)[] {
+    let i = this.agents[id][0]
+    let raumliste: (Room | undefined)[] = [undefined, undefined, undefined, undefined]
+    if ((isElem([i, i - this.g.breit, true], this.g.edges))) { raumliste[0] = this.g.rooms[i - this.g.breit][1].getRoom() }
+    if ((isElem([i, i + 1, true], this.g.edges))) { raumliste[1] = this.g.rooms[i + 1][1].getRoom() }
+    if ((isElem([i, i + this.g.breit, true], this.g.edges))) { raumliste[2] = this.g.rooms[i + this.g.breit][1].getRoom() }
+    if ((isElem([i, i - 1, true], this.g.edges))) { raumliste[3] = this.g.rooms[i - 1][1].getRoom() }
+    return raumliste
   }
 
-  move(direction: Direction, id:number):void {
+  move(direction: Direction, id: number): void {
     let player = this.agents[id]
     let i = this.agents[id][0]
 
     if (player[1] > 0 && !this.g.rooms[i][1].goal) {
-		let direction_to_check = -1
-		let next_room = -1
+      let direction_to_check = -1
+      let next_room = -1
 
-        if (direction == Direction.Up) {
-                let direction_to_check = 0;
-                let next_room = i-this.g.breit;
+      if (direction == Direction.Up) {
+        let direction_to_check = 0;
+        let next_room = i - this.g.breit;
+      }
+      else if (direction == Direction.Right) {
+        let direction_to_check = 1;
+        let next_room = i + 1;
+      }
+      else if (direction == Direction.Down) {
+        let direction_to_check = 2;
+        let next_room = i + this.g.breit;
+      }
+      else if (direction == Direction.Left) {
+        let direction_to_check = 3;
+        let next_room = i - 1;
+      }
+
+      if (this.neighbours(i)[direction_to_check]) {
+        player[0] = next_room // Betritt nächsten Raum
+        player[2] = player[2] + this.g.rooms[next_room][1].sword // Sammelt Schwert auf
+        this.g.rooms[next_room][1].sword = 0
+
+        if (player[2] >= this.g.rooms[next_room][1].monsterhp) {
+          player[2] -= this.g.rooms[next_room][1].monsterhp
+          this.g.rooms[next_room][1].monsterhp = 0
         }
-        else if (direction == Direction.Right) {
-                let direction_to_check = 1;
-                let next_room = i+1;
+        else {
+          this.g.rooms[next_room][1].monsterhp = this.g.rooms[next_room][1].monsterhp - player[2]
+          player[2] = 3
         }
-        else if (direction == Direction.Down) {
-                let direction_to_check = 2;
-                let next_room = i+this.g.breit;
-        }
-        else if (direction == Direction.Left) {
-                let direction_to_check = 3;
-                let next_room = i-1;
-        }
-            
-        if (this.neighbours(i)[direction_to_check]) {
-            player[0] = next_room // Betritt nächsten Raum
-            player[2] = player[2] + this.g.rooms[next_room][1].sword // Sammelt Schwert auf
-            this.g.rooms[next_room][1].sword = 0
-                    
-            if (player[2] >= this.g.rooms[next_room][1].monsterhp) {
-		player[2] -= this.g.rooms[next_room][1].monsterhp
-                this.g.rooms[next_room][1].monsterhp = 0
-            }
-            else {
-                this.g.rooms[next_room][1].monsterhp = this.g.rooms[next_room][1].monsterhp - player[2]
-		player[2] = 3
-            }
-            player[1] -= this.g.rooms[next_room][1].monsterhp  // wird vom monster geboxt 
-            }
-        }
+        player[1] -= this.g.rooms[next_room][1].monsterhp  // wird vom monster geboxt 
+      }
     }
+  }
 
-  getStatus(id:number):Status {
+  getStatus(id: number): Status {
     let player = this.agents[id]
     let isFinish = false
-    if (player[0] === (this.g.breit*this.g.ende) - 1) {
-        isFinish = true
+    if (player[0] === (this.g.breit * this.g.ende) - 1) {
+      isFinish = true
     }
     return new Status(this.g.roomNumToCoords(player[0]), player[1], player[2], isFinish)
-    }
+  }
 }
+let s = new Spiel(40, 30)
