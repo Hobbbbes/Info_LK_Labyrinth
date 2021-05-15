a9281f66fde183afc3ad109472f8b1054c776bd0
Calvin Katt <katt.calvin@gmail.com>
2021-05-15 15:04:34 +0000

    Complete GUI

--- index.ts
+++ index.ts
@@ -6,15 +6,16 @@ import {LeftWallStrategy} from "./strategy/LeftWallStrategy.js"
 import {AgressivStrategy} from "./strategy/AgressivStrategy.js"
 import {LeftWallStrategyAndMostlyPacifistStrategy} from "./strategy/LeftWallStrategyAndPacifist.js"
 import {RndStrategy} from "./strategy/RndStrategy.js"
-import { Direction } from "./strategy/Strategy.js";
+import { Direction, Strategy } from "./strategy/Strategy.js";
 import { Room } from "./raum.js";
 
 
 const canvas = document.querySelector('canvas[id="1"') as HTMLCanvasElement;
 const stats = document.getElementById("1.1") as HTMLParagraphElement;
+const StrategySelect = document.getElementById("strategy") as HTMLSelectElement;
+const coordParagraph = document.getElementById("1.2") as HTMLParagraphElement;
 canvas.width = 600;
 canvas.height = 600;
-
 const ctx = canvas.getContext("2d");
 ctx.fillStyle = "#a86b32";
 ctx.imageSmoothingEnabled = false;
@@ -36,8 +37,7 @@ var pAttack = new Player("sprites/adventurer-attack1-",270,170,4);
 var pRun = new Player("sprites/adventurer-run-",270,170,4)
 
 var S = new Spiel(50,50,20,20)
-var A = new Agent(S.createAgent(),50,50,S,new AgressivStrategy(50));
-
+var A : Agent
 var agentToRender = p;
 setInterval(() => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -56,16 +56,46 @@ document.body.appendChild(canvas);
 let startX = pRun.x
 let startY = pRun.y
 
+function NewNeigbours(){
+  var n = S.neighbours(A.ID);
+  if (n[0] != undefined){
+    up = new Room_Draw(n[0].Sword>0,n[0].Monster>0,up.x,up.y,1)
+  } else {
+    up = new Room_Draw(false,false,up.x,up.y)
+  }
+  if (n[1] != undefined){
+    right = new Room_Draw(n[1].Sword>0,n[1].Monster>0,right.x,right.y,2)
+  } else {
+    right = new Room_Draw(false,false,right.x,right.y)
+  }
+  if (n[2] != undefined){
+    down = new Room_Draw(n[2].Sword>0,n[2].Monster>0,down.x,down.y,3)
+  } else {
+    down = new Room_Draw(false,false,down.x,down.y)
+  }
+  if (n[3] != undefined){
+    left = new Room_Draw(n[3].Sword>0,n[3].Monster>0,left.x,left.y,4)
+  } else {
+    left = new Room_Draw(false,false,left.x,left.y)
+  }
+  var stat = S.getStatus(A.ID)
+  stats.innerText = "HP: " + stat.HP + "     SwordStrenght: " + stat.SwordStrenght;
+  coordParagraph.innerText = "x: " + stat.Pos[1] + "  y: " + stat.Pos[0];
+  if (stat.finish){
+    stats.innerText = "You found the exit";
+  }
+}
+
 function Step(){
   
-
+  console.log(StrategySelect.value)
   var dir = A.step();
   if (dir === false){
+    stats.innerText = "You died";
     return
   }
   agentToRender = pRun;
   dir = dir as Direction
-  var n = S.neighbours(A.ID);
   for(var i : number = 0; i<100;i++){
 
     setTimeout(() => {
@@ -84,29 +114,25 @@ function Step(){
     pRun.x = startX
     pRun.y = startY
     agentToRender = p
-    if (n[0] != undefined){
-      up = new Room_Draw(n[0].Sword>0,n[0].Monster>0,up.x,up.y,1)
-    } else {
-      up = new Room_Draw(false,false,up.x,up.y)
-    }
-    if (n[1] != undefined){
-      right = new Room_Draw(n[1].Sword>0,n[1].Monster>0,right.x,right.y,2)
-    } else {
-      right = new Room_Draw(false,false,right.x,right.y)
-    }
-    if (n[2] != undefined){
-      down = new Room_Draw(n[2].Sword>0,n[2].Monster>0,down.x,down.y,3)
-    } else {
-      down = new Room_Draw(false,false,down.x,down.y)
-    }
-    if (n[3] != undefined){
-      left = new Room_Draw(n[3].Sword>0,n[3].Monster>0,left.x,left.y,4)
-    } else {
-      left = new Room_Draw(false,false,left.x,left.y)
-    }
-    var stat = S.getStatus(A.ID)
-    stats.innerText = "HP: " + stat.HP + "     SwordStrenght: " + stat.SwordStrenght;
+    NewNeigbours();
+
   },16*100)
 
 }
+
+function newPlayer(){
+  var strategy : Strategy;
+  if(StrategySelect.value === "Agressiv"){
+    strategy = new AgressivStrategy(50);
+  } else if(StrategySelect.value === "LeftWall"){
+    strategy = new LeftWallStrategy(50);
+  } else if(StrategySelect.value === "LeftWallPacifist"){
+    strategy = new LeftWallStrategyAndMostlyPacifistStrategy(50);
+  } else {
+    strategy = new RndStrategy(50);
+  }
+  A = new Agent(S.createAgent(),50,50,S,strategy);
+  NewNeigbours();
+}
 document.querySelector('button[type=step]',).addEventListener('click',Step)
+document.querySelector('button[type=new_player]',).addEventListener('click',newPlayer)
